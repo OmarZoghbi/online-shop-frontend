592039bbdcdb1c24a92f7790d0418e633446014a
OmarZoghbi <omarzoghbi93@gmail.com>
2025-11-18 23:11:51 +0100

    import Vue from repo

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import ArticleList from './components/ArticleList.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <h1>Mein Online-Shop</h1>
+    <ArticleList />
+  </main>
+
+  <RouterView />
+</template>
+
+
+<style scoped>
+/* dein Style bleibt unverändert */
+</style>
---
+++ src/components/ArticleList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="article-list">
+    <h2>Artikelübersicht</h2>
+    <ul>
+      <li v-for="(article, index) in articles" :key="index">
+        <strong>{{ article.name }}</strong> – {{ article.description }} ({{ article.price }} €)
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArticleList",
+  data() {
+    return {
+      articles: [
+        { name: "Tastatur", description: "Mechanische Gaming-Tastatur", price: 59.99 },
+        { name: "Maus", description: "Kabellose Maus", price: 29.99 },
+        { name: "Monitor", description: "27 Zoll Full-HD", price: 189.99 },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.article-list {
+  margin-top: 20px;
+}
+h2 {
+  color: #42b883;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  margin: 6px 0;
+}
+</style>
